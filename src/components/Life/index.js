6d10e58dd5ac98312ab9771289ca39daab78b6aa
graylogo <graylogo@gmail.com>
2021-06-10 10:44:01 +0800

    生命周期

---
+++ src/components/Life/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+    // 3、重要的钩子
+    // render:初始化渲染或者更新渲染调用
+    // componentDidMount() :开启监听,发送ajax请求
+    // componentWillUnmount(): 做一些收尾工作,如:清理定时器
+export default class Life extends Component {
+    render() {
+        return (
+            <div>
+                 
+            </div>
+        )
+    }
+}
