a39890d0c9a530fefb689a74d220e59e4c94ad51
graylogo <graylogo@gmail.com>
2021-06-10 11:58:03 +0800

    换台电脑写

--- src/components/TdoList/List/index.js
+++ src/components/TdoList/List/index.js
@@ -5,7 +5,7 @@ export default class List extends Component {
     render() {
         return (
             <div>
-                
+                List
             </div>
         )
     }
--- src/components/TdoList/index.js
+++ src/components/TdoList/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './index.css'
-
+import Header from './Header'
+import List from './List'
+import Footer from './Footer'
 export default class TodoList extends Component {
     render() {
         return (
-            <div>
-                todo
+            <div className='todo-list'>
+               <Header/>
+               <List/>
+               <Footer/>
             </div>
         )
     }
