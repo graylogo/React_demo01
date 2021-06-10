5cbfd170299eb0fff8320d38bdb38698abbf694b
graylogo <graylogo@gmail.com>
2021-06-10 10:53:53 +0800

    TodoLList页面初始化完成

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,25 @@ import RefOld from './refs/RefOld'
 import GetByNo from './GetValue/GetByNo'
 import GetByYes from './GetValue/GetByYes'
 import FuncReturnFunc from './GetValue/FuncReturnFunc'
+import TodoList from './TdoList'
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <First/>
+                {/* 基础部分的页面 */}
+               <div style={{display: 'none'}}>
+               <First/>
                 <Second/>
                 <PropsBasic/>
                 <RefOld/>
                 <GetByNo/>
                 <GetByYes/>
                 <FuncReturnFunc/>
+               </div>
+               {/* todoList的页面 */}
+               <div>
+                   <TodoList/>
+               </div>
             </div>
         )
     }
---
+++ src/components/TdoList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                todo
+            </div>
+        )
+    }
+}
